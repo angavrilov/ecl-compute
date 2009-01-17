837b6182eb1066e37744ce8d28ea24632ee0e507
Alexander Gavrilov <angavrilov@gmail.com>
2009-01-17 19:30:48 +0300

    Implement index expression simplification.
    Sparse indexing support produces many expressions
    than can be trivially simplified. This implements it.
    
    I also fix a bug in the def-multivalue macro
    concerning the allocator, and lift the restriction
    on the mv-dimensions changes, since it is derived
    from indexes & layout.

--- index.lisp
+++ index.lisp
@@ -1,3 +1,5 @@
+(use-package :cl-match)
+
 (defun unsymbol (x)
     (if (symbolp x) (symbol-name x) x))
 
@@ -6,19 +8,93 @@
         (apply #'concatenate 'string
             (mapcar #'unsymbol items))))
 
+(defun simplify-index-1 (expr &optional (old-expr expr))
+    (match expr
+        ;; Expand abbreviations
+        (`(1+ ,x) `(+ ,x 1))
+        (`(1- ,x) `(- ,x 1))
+        ;; Collapse arithmetical no-ops
+        ((or `(/ ,x 1) `(* ,x 1)
+             `(+ ,x 0) `(- ,x 0) `(+ ,x))
+            x)
+        ((or `(* ,_ 0) `(/ 0 ,_)) 0)
+        (`(/ ,_ 0)
+            (error "Division by zero: ~A" old-expr))
+        ;; Calculate fully numerical expressions
+        ((when (every #'numberp args)
+             `(,(or '+ '- '* '/ 'mod 'rem 'truncate 'floor) ,@args))
+            (eval expr))
+        ;; Normalize sign
+        ((when (< v 0) `(+ ,x ,(type number v)))
+            `(- ,x ,(- v)))
+        ((when (< v 0) `(- ,x ,(type number v)))
+            `(+ ,x ,(- v)))
+        ;; Reorder the number in + and * to be last
+        ((when (not (numberp b))
+             `(,(as op (or '+ '*)) ,(type number a) ,b))
+            (list op b a))
+        ;; Simplify adjacent * & /
+        (`(,(as op (or '* '/))
+             (,op ,x ,(type integer i1))
+             ,(type integer i2))
+            (list op x (* i1 i2)))
+        ((when (= (mod n1 n2) 0)
+             `(/ (* ,x ,(type integer n1)) ,(type integer n2)))
+            `(* ,x ,(/ n1 n2)))
+        ;; Simplify adjacent + & -
+        (`(,(as op2 (or '+ '-))
+             (,(as op1 (or '+ '-)) ,x ,(type integer i1))
+             ,(type integer i2))
+            (let ((iv (if (eql op1 op2) (+ i1 i2) (- i1 i2))))
+                `(,op1 ,x ,iv)))
+        ;; Pull constants out of /
+        ((when (/= (truncate n1 n2) 0)
+             `(/ (,(as op (or '+ '-)) ,x ,(type integer n1))
+                  ,(type integer n2)))
+            (multiple-value-bind (divr remr) (truncate n1 n2)
+                `(,op (/ (,op ,x ,remr) ,n2) ,divr)))
+        ;; Push constants into *
+        ((when (/= (truncate n1 n2) 0)
+             `(,(as op (or '+ '-))
+                  (* ,x ,(type integer n2))
+                  ,(type integer n1)))
+            (multiple-value-bind (divr remr) (truncate n1 n2)
+                `(,op (* (,op ,x ,divr) ,n2) ,remr)))
+        ;; Remove trivial zero remainder case
+        ((when (= (mod mulv divv) 0)
+             `(,(or 'mod 'rem)
+                  (* _ ,(type integer mulv))
+                  ,(type integer divv)))
+            0)
+        ;; Nothing to do
+        (_ nil)))
+
+(defun simplify-rec (engine expr)
+    (if (atom expr)
+        expr
+        (let* ((rec-res (mapcar
+                            #'(lambda (sub) (simplify-rec engine sub))
+                            expr))
+               (subs-res (funcall engine rec-res expr)))
+            (if (null subs-res)
+                rec-res
+                (simplify-rec engine subs-res)))))
+
 (defun index-dimension (item)
     (destructuring-bind (iname minv maxv &key (by 1) (bands 1)) item
         (let ((fullrange `(/ (- ,maxv ,minv) ,by)))
-            (if (> bands 1)
-                (list bands `(1+ (/ ,fullrange ,bands)))
-                (list `(1+ ,fullrange))))))
+            (simplify-rec #'simplify-index-1
+                (if (> bands 1)
+                    (list bands `(1+ (/ ,fullrange ,bands)))
+                    (list `(1+ ,fullrange)))))))
 
 (defun index-refexpr (item)
     (destructuring-bind (iname minv maxv &key (by 1) (bands 1)) (car item)
         (let ((value `(/ (- ,(cdr item) ,minv) ,by)))
-            (if (> bands 1)
-                (list `(mod ,value ,bands) `(truncate (/ ,value ,bands)))
-                (list value)))))
+            (simplify-rec #'simplify-index-1
+                (if (> bands 1)
+                    (list `(mod ,value ,bands) `(floor ,value ,bands))
+                    (list value))))))
 
 (defun allocator-symbol (mv-name)
     (symcat "ALLOC-" mv-name))
@@ -50,9 +126,9 @@
              (declaim (type (array simple-float ,decl-dims) ,name))
              (set-prop-nochange ',name 'mv-indexes ',indexes)
              (set-prop-nochange ',name 'mv-layout ',layout)
-             (set-prop-nochange ',name 'mv-dimensions ',index-dims)
+             (setf (get ',name 'mv-dimensions) ',index-dims)
              (defun ,(allocator-symbol name) ()
-                 (make-array ,index-dims
+                 (make-array (list ,@index-dims)
                      :element-type 'simple-float
                      :initial-element 0.0))
              )))
@@ -84,7 +160,13 @@
                         name idxvals indexes))
                 `(aref ,name ,@idxlst)))))
 
+(defparameter M1 0)
+(defparameter N5 0)
+
 (pprint (macroexpand-1 '(def-multivalue HFIFi ((i 2 (1- N5) :bands 2) (k 2 (1- M1) :by 2)) :layout (k i))))
+
+(def-multivalue HFIFi ((i 2 (1- N5) :bands 2) (k 2 (1- M1) :by 2)) :layout (k i))
+
 (pprint (macroexpand-1 '(alloc-multivalues HFIFi)))
 (pprint (macroexpand-1 '(with-local-multivalues (HFIFi) foo)))
 (pprint (macroexpand-1 '(iref HFIFi 100 200)))
