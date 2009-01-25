671b22da3a879f7ab59ebdc71b28d309bb833c7f
Alexander Gavrilov <angavrilov@gmail.com>
2009-01-25 16:36:48 +0300

    Implement basic common subexpression elimination & code motion.
    Determine common sub-expressions and move them to separate
    let* wrappers, located at appropriate loop nesting levels.

--- compute.lisp
+++ compute.lisp
@@ -117,6 +117,114 @@
             `(safety-check ,checks ,new-expr)
             new-expr)))
 
+(defun count-subexprs-rec (expr cnt-table)
+    ;; Increment the counter, and avoid walking trees twice
+    (when (> (incf-nil (gethash expr cnt-table)) 1)
+        (return-from count-subexprs-rec nil))
+    (match expr
+        ((type atom var)
+            nil)
+        (`(ranging ,@_)
+            nil)
+        (`(loop-range ,_ ,@body)
+            (dolist (item body)
+                (count-subexprs-rec item cnt-table)))
+        (`(setf ,target ,val)
+            (dolist (titem (cdr target))
+                (count-subexprs-rec titem cnt-table))
+            (count-subexprs-rec val cnt-table))
+        (_
+            (dolist (item (cdr expr))
+                (count-subexprs-rec item cnt-table)))))
+
+(defun count-subexprs (expr)
+    (let ((table (make-hash-table :test #'equal)))
+        (count-subexprs-rec expr table)
+        table))
+
+(defun build-factor-table (cnt-table)
+    (let ((fct-table (make-hash-table :test #'equal)))
+        (maphash
+            #'(lambda (expr cnt)
+                  ;; Factor common subexpressions
+                  (when (> cnt 1)
+                      (setf (gethash expr fct-table) t))
+                  ;; Factor loop-invariant subexpressions
+                  (when (and (consp expr)
+                            (not (find (car expr)
+                                     '(ranging safety-check loop-range setf))))
+                      (let ((level (min-loop-level expr)))
+                          (dolist (sub (cdr expr))
+                              (unless (eql level (min-loop-level sub))
+                                  (setf (gethash sub fct-table) t))))))
+            cnt-table)
+        (maphash
+            #'(lambda (expr flag)
+                  ;; Don't factor constants and loop vars
+                  (when (match expr
+                            ((type number _) t)
+                            ('nil t)
+                            ((type symbol var) (get var 'mv-indexes))
+                            (`(ranging ,@_) (ranging-loop-level expr))
+                            (_ nil))
+                      (remhash expr fct-table)))
+            fct-table)
+        fct-table))
+
+(defun factor-vars-dumb (expr fct-table cur-level var-list nil-list)
+    (if (or (atom expr) (find (car expr) '(ranging)))
+        expr
+        (mapcar-save-old
+            #'(lambda (x) (factor-vars-rec x fct-table cur-level var-list nil-list))
+            expr)))
+
+(defun factor-vars-rec (expr fct-table cur-level var-list nil-list)
+    (match expr
+        (`(loop-range ,range ,@body)
+            (let* ((level (ranging-loop-level range))
+                   (vlist (cons nil var-list))
+                   (nbody (factor-vars-rec body fct-table level vlist nil-list)))
+                (unless (or (null cur-level) (= level (1- cur-level)))
+                    (error "Invalid loop nesting: ~A at level ~A" expr cur-level))
+                (if (car vlist)
+                    `(loop-range ,range (let* ,(nreverse (car vlist)) ,@nbody))
+                    `(loop-range ,range ,@nbody))))
+        (`(setf ,target ,val)
+            `(setf ,(factor-vars-dumb target fct-table cur-level var-list nil-list)
+                 ,(factor-vars-rec val fct-table cur-level var-list nil-list)))
+        (_
+            (let ((factor (gethash expr fct-table)))
+                (cond
+                    ((eql factor nil)
+                        (factor-vars-dumb expr fct-table cur-level var-list nil-list))
+                    ((eql factor t)
+                        (let* ((sym   (gensym))
+                               (nexpr (factor-vars-dumb expr fct-table cur-level var-list nil-list))
+                               (level (min-loop-level expr))
+                               (expr-pair (list sym nexpr)))
+                            (setf (get sym 'let-clause) expr-pair)
+                            (setf (get sym 'full-expr) expr)
+                            (setf (get sym 'loop-level) level)
+                            (cond
+                                ((eql level nil)
+                                    (push expr-pair (car nil-list)))
+                                ((and cur-level (>= level cur-level))
+                                    (push expr-pair (nth (- level cur-level) var-list)))
+                                (t
+                                    (error "Invalid level ~A at current ~A" level cur-level)))
+                            (setf (gethash expr fct-table) sym)))
+                    (t  factor))))))
+
+(defun factor-vars (expr fct-table)
+    (let* ((nil-list (list nil))
+           (nexpr    (factor-vars-rec expr fct-table nil nil nil-list)))
+        (if (car nil-list)
+            `(let* ,(nreverse (car nil-list)) ,nexpr)
+            nexpr)))
+
+(defun code-motion (expr)
+    (factor-vars expr (build-factor-table (count-subexprs expr))))
+
 (defmacro compute (name idxspec expr &key with)
     (let ((indexes    (get name 'mv-indexes))
           (layout     (get name 'mv-layout))
@@ -134,10 +242,15 @@
                (nolet-expr (expand-let loop-expr))
                (*consistency-checks* (make-hash-table :test #'equal))
                (noiref-expr (simplify-iref nolet-expr))
-               (check-expr  (insert-checks noiref-expr)))
-            check-expr)))
+               (check-expr  (insert-checks noiref-expr))
+               (motion-expr (code-motion check-expr)))
+            motion-expr)))
 
-(pprint (macroexpand-1 '(compute HFIFi (z k) (+ old 10) :with ((old (iref HFIFi z k))))))
-(pprint (macroexpand-1 '(compute HFIFi (z (+ k)) (+ (iref HFIFi z k) 10))))
+(setf var1 (macroexpand-1 '(compute HFIFi (z k) (+ old 10) :with ((old (iref HFIFi z k)))))
+      var2 (macroexpand-1 '(compute HFIFi (z (+ k 1)) (+ (iref HFIFi z (1+ k)) (iref HFIFi z (1+ k)) 10))))
+(pprint var1)
+(pprint var2)
 
-
+(defun xxx1 () (compute HFIFi (z k) (+ old 10) :with ((old (iref HFIFi z k)))))
+(defun xxx2 () (loop-indexes HFIFi ((k :skip-high 1))
+                   (compute HFIFi (z (+ k 2)) (+ (iref HFIFi z (+ k 2)) (iref HFIFi z (+ k 2)) 10))))
