777c96a1d2f2d375ba923818b4545a1cb9afb27e
Alexander Gavrilov <angavrilov@gmail.com>
2009-05-28 15:52:45 +0400

    Implement tree canonification and common factor extraction.
    Canonification gets rid of random argument permutations
    and ensures that equal => eql.
    Extraction converts (+ (* a b) (* a c)) to (* (+ b c) a).

--- expr-optimize.lisp
+++ expr-optimize.lisp
@@ -63,6 +63,13 @@
                           (copy-list lst))
                       (`(/ (* ,@lst))
                           (mapcar #'toggle-div lst))
+                      (`(- (* ,x ,@lst))
+                          (cons (toggle-minus x)
+                              (copy-list lst)))
+                      (`(- (/ (* ,x ,@lst)))
+                          (mapcar #'toggle-div
+                              (cons (toggle-minus x)
+                                  (copy-list lst))))
                       (_ (list arg))))
             args)))
 
@@ -98,6 +105,7 @@
     (match expr
         (`(ranging ,@_) old-expr)
         (`(- (- ,x)) x)
+        (`(/ (- ,x)) `(- (/ ,x)))
         (`(- ,(type number val)) (- val))
         (`(* ,@args)
             (let* ((minus-cnt 0)
@@ -135,6 +143,143 @@
                     `(- (+ ,@(mapcar #'- nums)
                            ,@(mapcar #'second non-nums))))))
         (_ nil)))
+
+(defparameter *canonify-cache* (make-hash-table :test #'equal))
+
+(defun canonify-compare (expr1 expr2)
+    (cond
+        ((numberp expr1)
+            (if (numberp expr2)
+                (< expr1 expr2)
+                t))
+        ((numberp expr2)
+            nil)
+        (t
+            (< (sxhash expr1) (sxhash expr2)))))
+
+(defun common-sublist (list1 list2)
+    (cond
+        ((null list1) nil)
+        ((null list2) nil)
+        ((eql (car list1) (car list2))
+            (cons (car list1)
+                (common-sublist (cdr list1) (cdr list2))))
+        ((canonify-compare (car list1) (car list2))
+            (common-sublist (cdr list1) list2))
+        ((canonify-compare (car list2) (car list1))
+            (common-sublist (cdr list1) list2))
+        (t
+            (common-sublist (cdr list1) (cdr list2)))))
+
+(defun subtract-list (list1 list2)
+    (cond
+        ((null list2) list1)
+        ((null list1)
+            (error "Cannot subtract ~A from NIL" list2))
+        ((eql (car list1) (car list2))
+            (subtract-list (cdr list1) (cdr list2)))
+        ((canonify-compare (car list1) (car list2))
+            (cons (car list1)
+                (subtract-list (cdr list1) list2)))
+        (t
+            (error "Cannot subtract ~A from ~A" list2 list1))))
+
+(defun canonify-expr (expr)
+    (if (null expr)
+        expr
+        (let* ((sorted-expr
+                   (match expr
+                       (`(,(as op (or '+ '*)) ,@rest)
+                           (cons op
+                               (stable-sort (copy-list rest)
+                                   #'canonify-compare)))
+                       (_ expr)))
+               (cached-expr
+                   (gethash sorted-expr *canonify-cache*)))
+            (or cached-expr
+                (setf
+                    (gethash sorted-expr *canonify-cache*)
+                    sorted-expr)))))
+
+(defun canonify-tree-1 (expr old-expr)
+    (if (and (consp expr)
+             (eql (car expr) 'ranging))
+        old-expr
+        (canonify-expr expr)))
+
+(defun canonify-tree (expr)
+    (simplify-rec-once #'canonify-tree-1 expr))
+
+(defun pull-factor-list (init-lst to-pull)
+    (let ((diff (subtract-list init-lst to-pull)))
+        (cond
+            ((null diff)
+                1)
+            ((null (cdr diff))
+                (car diff))
+            (t
+                (canonify-expr `(* ,@diff))))))
+
+(defun pull-factors-1 (expr old-expr)
+    ;; Requires and preserves canonification
+    (match expr
+        (`(ranging ,@_) old-expr)
+        (`(+ ,@args)
+            (let ((common-prod
+                       (reduce
+                           #'(lambda (cur-set arg)
+                                 (let ((fct-list
+                                           (match arg
+                                               (`(* ,@lst)
+                                                   lst)
+                                               (`(- (* ,@lst))
+                                                   lst)
+                                               (`(- ,x)
+                                                   (list x))
+                                               (_
+                                                   (list arg)))))
+                                     (if (eql cur-set 'none)
+                                         fct-list
+                                         (common-sublist
+                                             cur-set
+                                             fct-list))))
+                           args
+                           :initial-value 'none)))
+                (if (or (null common-prod)
+                        (eql common-prod 'none))
+                    (if (eql expr old-expr) old-expr
+                        (canonify-expr expr))
+                    (let ((filtered-args
+                              (mapcar
+                                  #'(lambda (arg)
+                                        (match arg
+                                            (`(* ,@lst)
+                                                (pull-factor-list
+                                                    lst common-prod))
+                                            (`(- (* ,@lst))
+                                                (canonify-expr
+                                                    `(- ,(pull-factor-list
+                                                            lst common-prod))))
+                                            (`(- ,x)
+                                                (canonify-expr
+                                                    `(- ,(pull-factor-list
+                                                            (list x) common-prod))))
+                                            (_
+                                                (pull-factor-list
+                                                    (list arg) common-prod))))
+                                  args)))
+                        (canonify-expr
+                            `(* ,(canonify-expr
+                                     (flatten+ filtered-args))
+                                ,@common-prod))))))
+        ((when (not (eql expr old-expr))
+            `(* ,@args))
+            (canonify-expr
+                (flatten* args)))
+        (_
+            (if (eql expr old-expr) old-expr
+                (canonify-expr expr)))))
+
 
 (defun split-parts (lst)
     (let* ((len (length lst))
