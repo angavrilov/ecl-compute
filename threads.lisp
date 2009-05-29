99389d0e1e49fcfa87b3cc578d4fd8a1004cb9f8
Alexander Gavrilov <angavrilov@gmail.com>
2009-05-29 19:48:05 +0400

    Use a task ID instead of the reset condition.
    Reduce blocking interactions by using a task index check
    instead of the reset condition for avoiding duplicate
    task startups.

--- threads.lisp
+++ threads.lisp
@@ -7,37 +7,37 @@
 (defvar *worker-mutex*    (mp:make-lock))
 (defvar *work-start-cond* (mp:make-condition-variable))
 (defvar *work-done-cond*  (mp:make-condition-variable))
-(defvar *work-reset-cond*  (mp:make-condition-variable))
 
 (defvar *working-threads* 0)
+(defvar *task-id*         0)
 (defvar *task-function*   nil)
 
 (defun worker-thread (idx)
     (format t "Worker ~A starting.~%" idx)
     (unwind-protect
-        (loop
-            (multiple-value-bind
-                    (task w-count)
-                    (mp:with-lock (*worker-mutex*)
-                        (unless *task-function*
-                            (mp:condition-variable-wait
-                                *work-start-cond* *worker-mutex*))
-                        (when (> idx *worker-count*)
-                            (return-from worker-thread))
-                        (values *task-function* *worker-count*))
-                (when task
-                    (unwind-protect
-                        (handler-case
-                            (funcall task idx (1+ w-count))
-                            (error (err)
-                                (format t "Worker ~A failed:~%  ~A~%" idx err)))
+        (let ((last-id 0))
+            (loop
+                (multiple-value-bind
+                        (task task-id w-count)
                         (mp:with-lock (*worker-mutex*)
-                            (incf *working-threads* -1)
-                            (when (<= *working-threads* 0)
-                                (mp:condition-variable-broadcast *work-done-cond*))
-                            (when *task-function*
+                            (unless (and *task-function*
+                                         (/= *task-id* last-id))
                                 (mp:condition-variable-wait
-                                    *work-reset-cond* *worker-mutex*)))))))
+                                    *work-start-cond* *worker-mutex*))
+                            (when (> idx *worker-count*)
+                                (return-from worker-thread))
+                            (values *task-function* *task-id* *worker-count*))
+                    (when (and task (/= task-id last-id))
+                        (setf last-id task-id)
+                        (unwind-protect
+                            (handler-case
+                                (funcall task idx (1+ w-count))
+                                (error (err)
+                                    (format t "Worker ~A failed:~%  ~A~%" idx err)))
+                            (mp:with-lock (*worker-mutex*)
+                                (incf *working-threads* -1)
+                                (when (<= *working-threads* 0)
+                                    (mp:condition-variable-broadcast *work-done-cond*))))))))
         (format t "Worker ~A exited.~%" idx)))
 
 (defun spawn-worker-threads (num)
@@ -57,9 +57,12 @@
     (spawn-worker-threads (max (1- num) 0)))
 
 (defun run-work (fun)
+    (when (null fun)
+        (error "Cannot run a NIL task"))
     (mp:with-lock (*worker-mutex*)
         (when *task-function*
             (error "Task already running"))
+        (incf *task-id*)
         (setf *task-function* fun)
         (setf *working-threads* *worker-count*)
         (mp:condition-variable-broadcast *work-start-cond*))
@@ -69,8 +72,7 @@
             (when (> *working-threads* 0)
                 (mp:condition-variable-wait
                     *work-done-cond* *worker-mutex*))
-            (setf *task-function* nil)
-            (mp:condition-variable-broadcast *work-reset-cond*))))
+            (setf *task-function* nil))))
 
 (defun wrap-parallel (range code &optional (gen-func #'identity))
     (destructuring-bind
