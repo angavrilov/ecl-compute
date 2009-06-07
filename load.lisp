b8cc1a1b652cce5ce65a102b3a828c9564c09a49
Alexander Gavrilov <angavrilov@gmail.com>
2009-06-07 13:58:19 +0400

    Compile the core of the multithreading engine.
    Most of the optimizer code is executed only during
    compilation, so its performance does not matter much.
    The threading code, however, does run during actual
    execution.

--- load.lisp
+++ load.lisp
@@ -1,19 +1,33 @@
-(dolist (module-name
-            '("formula.lisp"
-              "compute-pkg.lisp"
-              "expr-index.lisp"
-              "expr-optimize.lisp"
-              "expr-refactor.lisp"
-              "expr-types.lisp"
-              "multivalue.lisp"
-              "threads.lisp"
-              "compute.lisp"
-              "compile-sse.lisp"))
-     (load (merge-pathnames module-name *load-pathname*)))
+(labels ((load-interp (name)
+             (load (merge-pathnames name *load-pathname*)))
+         (load-compile (name)
+             (unless (load
+                         (merge-pathnames
+                             (concatenate 'string name ".fas")
+                             *load-pathname*)
+                         :if-does-not-exist nil)
+                 (compile-file
+                     (merge-pathnames
+                         (concatenate 'string name ".lisp")
+                         *load-pathname*)
+                     :load t))))
 
-(unless (load
-            (merge-pathnames "utils.fas" *load-pathname*)
-            :if-does-not-exist nil)
-    (compile-file
-        (merge-pathnames "utils.lisp" *load-pathname*)
-        :load t))
+    (dolist (module-name
+                '("formula.lisp"
+                  "compute-pkg.lisp"
+                  "expr-index.lisp"
+                  "expr-optimize.lisp"
+                  "expr-refactor.lisp"
+                  "expr-types.lisp"
+                  "multivalue.lisp"))
+        (load-interp module-name))
+
+    (load-compile "thread-core")
+
+    (dolist (module-name
+                '("threads.lisp"
+                  "compute.lisp"
+                  "compile-sse.lisp"))
+        (load-interp module-name))
+
+    (load-compile "utils"))
