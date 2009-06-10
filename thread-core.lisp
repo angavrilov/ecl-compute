d7d649885b9cabfe27f17fe74bef0f2b8fd2f38c
Alexander Gavrilov <angavrilov@gmail.com>
2009-06-10 17:18:21 +0400

    Support locking thread affinity.
    Implement support for binding threads to specific execution
    cores, and allow using a default thread count, based on the
    value of the OMP_NUM_THREADS environment variable and CPU
    core count.

--- thread-core.lisp
+++ thread-core.lisp
@@ -2,6 +2,48 @@
 
 (in-package fast-compute)
 
+#+linux (progn
+    (ffi:clines "#include <unistd.h>")
+
+    (defun get-cpu-count ()
+        (max 1
+            (ffi:c-inline () () :int
+                "sysconf(_SC_NPROCESSORS_ONLN)"
+                :one-liner t)))
+
+    (ffi:clines "#include <sched.h>")
+    (ffi:clines "#include <errno.h>")
+    (ffi:clines "#include <string.h>")
+
+    (defun set-cpu-affinity (cpus)
+        (let ((cpu-list (if (listp cpus) cpus (list cpus)))
+              (numcpus (get-cpu-count)))
+            (unless cpus
+                (error "Cannot set affinity to an empty list"))
+            (dolist (item cpu-list)
+                (unless (and (integerp item) 
+                             (<= 0 item)
+                             (< item numcpus))
+                    (error "Invalid CPU index in affinity: ~A" item)))
+            (ffi:c-inline (cpu-list) (:object) :void
+                "cpu_set_t cset;
+                 CPU_ZERO(&cset);
+                 cl_object lst = #0;
+                 while (CONSP(lst)) {
+                   CPU_SET(fixnnint(ECL_CONS_CAR(lst)), &cset);
+                   lst = ECL_CONS_CDR(lst);
+                 }
+                 if (sched_setaffinity(0,sizeof(cset),&cset) < 0)
+                   FEerror(\"Affinity setting failed: ~A\", 1,
+                           make_base_string_copy(strerror(errno)));
+                ")))
+)
+
+#-linux (progn
+    (defun get-cpu-count () 1)
+    (defun set-cpu-affinity (cpus) (declare (ignore cpus)) nil)
+)
+
 (defvar *worker-count*    0)
 (defvar *worker-threads*  ())
 (defvar *worker-mutex*    (mp:make-lock))
@@ -53,9 +95,6 @@
                     #'worker-thread *worker-count*)
                 *worker-threads*))))
 
-(defun set-compute-thread-count (num)
-    (spawn-worker-threads (max (1- num) 0)))
-
 (defun run-work (fun)
     (when (null fun)
         (error "Cannot run a NIL task"))
@@ -73,3 +112,15 @@
                 (mp:condition-variable-wait
                     *work-done-cond* *worker-mutex*))
             (setf *task-function* nil))))
+
+(defun set-compute-thread-count (num-threads &key (adjust-affinity t))
+    (let ((num-threads (or num-threads
+                           (ignore-errors (parse-integer (si:getenv "OMP_NUM_THREADS")))
+                           (get-cpu-count))))
+        (spawn-worker-threads (max (1- num-threads) 0)))
+    (when adjust-affinity
+        (let ((cpu-count (get-cpu-count)))
+            (run-work
+                #'(lambda (idx num)
+                      (set-cpu-affinity (- cpu-count 1 (mod idx cpu-count))))))))
+
