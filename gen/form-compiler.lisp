56a2bb5de34c09f93c8d6da427b6e788a837c786
Alexander Gavrilov <angavrilov@gmail.com>
2009-08-16 15:54:48 +0400

    Refactor the form compiler framework.
    Since specifying the standard &allow-other-keys keyword
    disables errors about unknown keys, there is no need to
    reinvent the wheel. Make the form compilers use the normal
    key parameters.

--- gen/form-compiler.lisp
+++ gen/form-compiler.lisp
@@ -6,78 +6,74 @@
 (defparameter *cg-type-table* nil)
 
 (defun get-inline-tag (idx)
-    (aref #("0" "1" "2" "3" "4" "5" "6" "7" "8" "9"
-            "a" "b" "c" "d" "e" "f" "g" "h" "i" "j"
-            "k" "l" "m" "n" "o" "p" "q" "r" "s" "t"
-            "u" "v" "w" "x" "y" "z") idx))
+  (aref #("0" "1" "2" "3" "4" "5" "6" "7" "8" "9"
+          "a" "b" "c" "d" "e" "f" "g" "h" "i" "j"
+          "k" "l" "m" "n" "o" "p" "q" "r" "s" "t"
+          "u" "v" "w" "x" "y" "z") idx))
 
 (defun get-factored-cg-type (expr)
-    (gethash (unwrap-factored expr) *cg-type-table*))
+  (gethash (unwrap-factored expr) *cg-type-table*))
 
 (defun do-make-form-compiler (form-arg args body)
-   `(let ((,form-arg cg-form)
-          ,@(mapcar
-                #'(lambda (sym)
-                      `(,sym
-                           ,(if (equal (symbol-name sym)
-                                    (format nil "~A-TYPE" form-arg))
-                                '(if *cg-type-table*
-                                     (gethash cg-form *cg-type-table*))
-                                'nil)))
-                args))
-        (declare (ignorable ,form-arg))
-        ,@(if args
-              `((do ((aptr cg-flags (cddr aptr)))
-                    ((null aptr))
-                    (case (car aptr)
-                        ,@(mapcar
-                              #'(lambda (sym)
-                                    `(,(intern (symbol-name sym) "KEYWORD")
-                                         (setf ,sym (cadr aptr))))
-                              args)))))
-        (macrolet
-                ((text (pattern &rest args)
-                     (if (and (stringp pattern)
-                             (or args
-                                 (position #\~ pattern)))
-                         `(format cg-output (formatter ,pattern) ,@args)
-                         (progn
-                             (assert (null args))
-                             `(write-string ,pattern cg-output))))
-                 (recurse (expr &rest flags)
-                     (let ((use-stack 'cg-full-stack))
-                         (when (find (car flags)
-                                   '(:use-stack :switch-stack))
-                             (setf use-stack (cadr flags))
-                             (setf flags (cddr flags)))
+  (let* ((text-sym        (intern "TEXT"))
+         (recurse-sym     (intern "RECURSE"))
+         (recurse-str-sym (intern "RECURSE-STR"))
+         (arg-keys (mapcar #'(lambda (sym)
+                               (if (and (symbolp sym)
+                                        (equal (symbol-name sym)
+                                               (format nil "~A-TYPE" form-arg)))
+                                   `(,sym (if *cg-type-table*
+                                              (gethash cg-form *cg-type-table*)))
+                                   sym))
+                           args)))
+    (values `(cg-full-stack cg-cur-stack cg-output cg-form &rest cg-flags
+                            ,@(if arg-keys
+                                  `(&key ,@arg-keys &allow-other-keys)))
+            `(let ((,form-arg cg-form))
+               (declare (ignorable ,form-arg))
+               (macrolet
+                   ((,text-sym (pattern &rest args)
+                      (if (and (stringp pattern)
+                               (or args (position #\~ pattern)))
+                          `(format cg-output (formatter ,pattern) ,@args)
+                          (progn
+                            (assert (null args))
+                            `(write-string ,pattern cg-output))))
+                    (,recurse-sym (expr &rest flags)
+                      (let ((use-stack 'cg-full-stack))
+                        (when (find (car flags) '(:use-stack :switch-stack))
+                          (setf use-stack (cadr flags))
+                          (setf flags (cddr flags)))
                         `(let ((stack ,use-stack))
-                             (funcall (car stack)
-                                 stack (cdr stack)
-                                 cg-output ,expr (list ,@flags)))))
-                 (recurse-str (&rest args)
-                     `(with-output-to-string (cg-output)
-                          (recurse ,@args))))
-            (match cg-form
-                ,@body
-                (_
+                           (funcall (car stack)
+                                    stack (cdr stack)
+                                    cg-output ,expr ,@flags))))
+                    (,recurse-str-sym (&rest args)
+                      `(with-output-to-string (cg-output)
+                         (,',recurse-sym ,@args))))
+                 (match cg-form
+                   ,@body
+                   (_
                     (if cg-cur-stack
-                        (funcall (car cg-cur-stack)
-                            cg-full-stack (cdr cg-cur-stack)
-                            cg-output cg-form cg-flags)
-                        (error "Cannot compile, unrecognized form: ~A" cg-form)))))))
+                        (apply (car cg-cur-stack)
+                               cg-full-stack (cdr cg-cur-stack)
+                               cg-output cg-form cg-flags)
+                        (error "Cannot compile, unrecognized form: ~A" cg-form)))))))))
 
 (defmacro form-compiler (arg-set &body body)
-    (assert (consp arg-set))
-    `#'(lambda (cg-full-stack cg-cur-stack cg-output cg-form cg-flags)
-           ,(do-make-form-compiler (car arg-set) (cdr arg-set) body)))
+  (assert (consp arg-set))
+  (multiple-value-bind (arg-spec code)
+      (do-make-form-compiler (car arg-set) (cdr arg-set) body)
+    `#'(lambda ,arg-spec ,code)))
 
 (defmacro def-form-compiler (name arg-set &body body)
-    (assert (consp arg-set))
-    `(defun ,name (cg-full-stack cg-cur-stack cg-output cg-form cg-flags)
-         ,(do-make-form-compiler (car arg-set) (cdr arg-set) body)))
+  (assert (consp arg-set))
+  (multiple-value-bind (arg-spec code)
+      (do-make-form-compiler (car arg-set) (cdr arg-set) body)
+    `(defun ,name ,arg-spec ,code)))
 
 (defun call-form-compilers (stack form &rest flags)
-    (with-output-to-string (out)
-        (funcall (car stack)
-            stack (cdr stack) out
-            form flags)))
+  (with-output-to-string (out)
+    (apply (car stack)
+           stack (cdr stack) out
+           form flags)))
